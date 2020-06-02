466e9557b7ca8270eb6f41f32e6d0eb6ab65966a
Ted NIVAN <ted.jp.nivan@gmail.com>
2020-06-02 21:34:52 +0200

    Removed AES Encryption

--- iot-device.ino
+++ iot-device.ino
@@ -1,17 +1,12 @@
 #include <SoftwareSerial.h>
-#include <AESLib.h>
 
 SoftwareSerial BTserial(10, 11);
-
-const uint8_t key[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
 void setup()
 {
     BTserial.begin(9600);
   
-    char data[] = "70              ";
-    
-    aes128_enc_single(key, data);
+    char data[] = "70";
 
     for (int i = 0; i < sizeof(data) - 1; i++)
     {
